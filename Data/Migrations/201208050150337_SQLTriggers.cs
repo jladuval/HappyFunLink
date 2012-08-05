cfb3a93f5edf100faaf58807e4ed78f3e9a07a49
JDuval <jladuval@gmail.com>
2012-08-05 14:45:40 +1200

    Added trigger for generating happy links

---
+++ Data/Migrations/201208050150337_SQLTriggers.cs
@@ -0,0 +1,19 @@
+namespace Data.Migration
+{
+    using System.Data.Entity.Migrations;
+    
+    public partial class SQLTriggers : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("CREATE TRIGGER HappyLinkGenAdj On Adjectives AFTER INSERT AS MERGE INTO HappyLinks As HL USING (SELECT Adjectives.Word + ' ' + Copy.Word + ' ' + Nouns.Word FROM Adjectives INNER JOIN Adjectives AS [Copy] ON Adjectives.Word != Copy.Word CROSS JOIN Nouns) AS Source(Word) ON (HL.Word) = (Source.Word) WHEN NOT MATCHED THEN INSERT (Word, LastAccessed) VALUES (Source.Word, NULL);");
+            Sql("CREATE TRIGGER HappyLinkGenNoun On Nouns AFTER INSERT AS MERGE INTO HappyLinks As HL USING (SELECT Adjectives.Word + ' ' + Copy.Word + ' ' + Nouns.Word FROM Adjectives INNER JOIN Adjectives AS [Copy] ON Adjectives.Word != Copy.Word CROSS JOIN Nouns) AS Source(Word) ON (HL.Word) = (Source.Word) WHEN NOT MATCHED THEN INSERT (Word, LastAccessed) VALUES (Source.Word, NULL);");
+        }
+        
+        public override void Down()
+        {
+            Sql("Drop trigger HappyLinkGenAdj");
+            Sql("Drop trigger HappyLinkGenNoun");
+        }
+    }
+}
