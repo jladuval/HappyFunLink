6f738e587e06bbbd18bae8413c8d9190fdf7df12
Simon Frost <simonhfrost@gmail.com>
2013-03-17 12:58:42 +1300

    Basic server side validation

--- HappyFunLink/Models/Home/LinkModel.cs
+++ HappyFunLink/Models/Home/LinkModel.cs
@@ -4,7 +4,9 @@
 
     public class LinkModel
     {
-        [Required]
+        [Required(ErrorMessage="Please enter a link")]
+        [StringLength(1000, ErrorMessage = "Link must be under 1000 characters")]
+        [RegularExpression(@"^[\S]*$", ErrorMessage = "Invalid characters in link")] // No spaces
         public string OriginalLink { get; set; }
 
         public string HappyLink { get; set; }
